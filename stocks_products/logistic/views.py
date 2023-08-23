dfcf3c8d3120b211a4a0f61bdfa1ebe6184524f1
XiXnice <kuzminov399@yandex.ru>
2023-08-24 00:45:37 +0300

    first commit

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,18 @@
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework import filters
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filter_fields = ['address', 'products']
